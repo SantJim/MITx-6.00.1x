3a7f6f7a78c7d849e8459ce46c4b3f2e532c83cf
SantJim <xaxsant@gmail.com>
2018-05-17 18:24:01 -0400

    Update alphabet_substring.py

--- alphabet_substring.py
+++ alphabet_substring.py
@@ -1,10 +1,16 @@
-s = 'azcbobobegghakl'
-
-numVowels = 0
+substring_len = 0
 substring = s[0]
 for i in range(len(s) - 1 ):
     if s[i] <= s[i+1]:
         substring = substring + s[i+1]
+        if len(substring) > substring_len: 
+            tempstring = substring
+            substring_len = len(substring)
     else:
         substring = s[i+1]
-print(substring)
+        
+if substring_len == 0:
+    print("Longest substring in alphabetical order is: " + substring)
+else:
+    print("Longest substring in alphabetical order is: " + tempstring)
+ 
